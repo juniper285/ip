897df9cf9da01fed37cba6b5eb8af8ab7050ee4a
ALYSSUM\chloe <e1155349@u.nus.edu>
2025-01-28 01:21:41 +0800

    Add: save file

--- src/main/java/Events.java
+++ src/main/java/Events.java
@@ -3,11 +3,20 @@ public class Event extends Task{
     protected boolean complete;
     protected String startTime;
     protected String endTime;
+    protected boolean isComplete;
 
     public Event(String taskName, String startTime, String endTime) {
         super(taskName);
         this.startTime = startTime;
         this.endTime = endTime;
+        this.isComplete = false;
+    }
+
+    public Event(String taskName, String startTime, String endTime, boolean isComplete) {
+        super(taskName);
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isComplete = isComplete;
     }
 
     public String getStatusIcon() {
