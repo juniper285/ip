bdaaa5429f8b024d52a9c82347793c6eda29e8fa
ALYSSUM\chloe <e1155349@u.nus.edu>
2025-01-31 15:41:23 +0800

    More LocalDate formats

---
+++ src/main/java/Events.java
@@ -0,0 +1,56 @@
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class Events extends Task{
+    protected String taskName;
+    protected boolean complete;
+    protected LocalDate startTime;
+    protected LocalDate endTime;
+
+    public Events(String taskName, LocalDate startTime, LocalDate endTime) {
+        super(taskName);
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.complete = false;
+    }
+
+    public Events(String taskName, LocalDate startTime, LocalDate endTime, boolean isComplete) {
+        super(taskName);
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.isComplete = isComplete;
+    }
+
+    public Events(String taskName) {
+
+        super(taskName);
+    }
+
+    public String getStatusIcon() {
+
+        return "[E]" + super.getStatusIcon(); // mark done task with X
+    }
+
+    void markDone(int index) {
+
+        super.markDone(index);
+    }
+
+    void markUndone(int index) {
+
+        super.markUndone(index);
+    }
+
+    public String convertToText() {
+        return "E | " + super.convertToText() + " | "
+                + this.startTime.format(DateTimeFormatter.ofPattern("d MMM yyyy")) + " | "
+                + this.endTime.format(DateTimeFormatter.ofPattern("d MMM yyyy"));
+    }
+
+    public String toString() {
+        return super.toString() + " (from: " + this.startTime.toString() + " to: " + this.endTime + ")";
+    }
+}
+
+
